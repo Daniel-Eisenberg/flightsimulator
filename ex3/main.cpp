ecdeeed8af9cc3984bbaffd08e89e0eb5cb365e9
yuvalgrin <juvs2k@gmail.com>
2019-12-23 15:53:13 +0200

    added lexer method

--- ex3/main.cpp
+++ ex3/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <string>
 #include "Tcp_Server.h"
 #include "Client_Side.h"
-int main() {
+
+using namespace std;
+
+void lexerCode(string filename) {
+//iterate all line;
+//split by?
+}
+
+int main(int argc, char *argv[]) {
 
     Tcp_Server server;
     server.create_socket();
-    
+    std::string filename = *argv;
+    lexerCode(filename);
+
     return 0;
 }
+
