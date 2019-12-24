80b6da4b01368dd017402a69b12113820434b1e9
Daniel-Eisenberg <dni1250@gmail.com>
2019-12-25 00:27:43 +0200

    lexer is working, need to make the code lok better

--- ex3/main.cpp
+++ ex3/main.cpp
@@ -46,6 +46,19 @@ void removeSpaces(char *str) {
     str[count] = '\0';
 }
 
+void removeTabs(char *str) {
+    // To keep track of non-tab character count
+    int count = 0;
+
+    // Traverse the given string. If current character
+    // is not tab, then place it at index 'count++'
+    for (int i = 0; str[i]; i++)
+        if (str[i] != '\t')
+            str[count++] = str[i]; // here count is
+    // incremented
+    str[count] = '\0';
+}
+
 string removeclosing(string str) {
     vector<string> s = split(str, '(');
     vector<string> temp = split(s.at(1), ')');
@@ -53,31 +66,36 @@ string removeclosing(string str) {
 }
 
 string find_operator(string str) {
-    if (str.find("!=")) {
+    if (str.find("!=") != std::string::npos) {
         return "!=";
-    } else if (str.find("==")) {
+    } else if (str.find("==") != std::string::npos) {
         return "==";
-    } else if (str.find(">=")) {
+    } else if (str.find(">=") != std::string::npos) {
         return ">=";
-    } else if (str.find("<=")) {
+    } else if (str.find("<=") != std::string::npos) {
         return "<=";
-    } else if (str.find("<")) {
+    } else if (str.find("<") != std::string::npos) {
         return "<";
-    } else if (str.find("<")) {
+    } else if (str.find("<") != std::string::npos) {
         return "<";
     } else return "error";
 }
 
-void lexerCode(std::string filename) {
+vector<string> lexerCode(std::string filename) {
 
     vector<string> params;
     vector<string> temp;
     fstream file;
-    file.open(filename);
-    string line;
+    file.open(filename, std::fstream::in);
+    if (!file.is_open()) {
+        throw "didnt read file";
+    }
+    string line, debug_line;
     while (getline(file, line)) {
         char string1[1024];
         strcpy(string1, line.c_str());
+        removeTabs(string1);
+        debug_line = string1;
         removeSpaces(string1);
         line = string1;
         if (line.rfind("openDataServer", 0) == 0) {
@@ -96,19 +114,18 @@ void lexerCode(std::string filename) {
             params.push_back("var");
             unsigned i;
             for (i = 3; i < line.length(); i++) {
+                char a = line.at(i);
                 if (line.at(i) == '-' || line.at(i) == '<' || line.at(i) == '=') {
                     break;
                 }
             }
-            params.push_back(line.substr(3, (3 - i)));
+            params.push_back(line.substr(3, (i - 3)));
             if ((line.find("->") != std::string::npos)) {
                 params.push_back("->");
                 params.push_back("sim");
                 unsigned long start_pos = line.find('(');
                 unsigned long end_pos = line.find(')');
                 params.push_back(line.substr(start_pos + 1, end_pos - 1));
-
-
             } else if (line.find("<-") != std::string::npos) {
                 params.push_back("<-");
                 params.push_back("sim");
@@ -117,42 +134,69 @@ void lexerCode(std::string filename) {
                 params.push_back(line.substr(start_pos + 1, end_pos - 1));
             } else {
                 params.push_back("=");
-                check_equal_with_yuval;
+                vector<string> temp = split(line, '=');
+                params.push_back("$");
+                params.push_back(temp.at(1));
+                params.push_back("$");
             }
+        } else if (line.rfind("while", 0) == 0 || line.rfind("if", 0) == 0) {
+            string condition;
+            if (line.rfind("while", 0) == 0) {
+                condition = "while";
+            } else {
+                condition = "if";
+            }
+            params.push_back(condition);
+            string str = find_operator(line);
+            vector<string> param = split(line, condition);
+            param = split(param.at(0), str);
+            params.push_back("$");
+            params.push_back(param.at(0));
+            params.push_back("$");
+            params.push_back(str);
+            params.push_back("$");
+            params.push_back(split(param.at(1), '{').at(0));
+            params.push_back("$");
+            params.push_back("{");
         } else if (line.find('=') != string::npos) {
             vector<string> temp = split(line, '=');
             params.push_back(temp.at(0));
             params.push_back("=");
-            check_equal_with_yuval;
-        } else if (line.rfind("while", 0) == 0 || line.rfind("if", 0) == 0) {
-            string str = find_operator(line);
-            vector<string> param = split(line, str);
+            params.push_back("$");
+            params.push_back(temp.at(1));
+            params.push_back("$");
+        }  else if (line.rfind("Print", 0) == 0) {
+            vector<string> param = split(debug_line, '(');
             params.push_back(param.at(0));
-            params.push_back(str);
-            string boolean_string = param.at(1);
-            check_equal_with_yuval;
-            params.push_back("{");
-        } else if (line.rfind("Print", 0) == 0) {
-            vector<string> param = split(line, '(');
-            params.push_back(param.at(0));
-            params.push_back(removeclosing(param.at(1)));
+            params.push_back(split(param.at(1), ')').at(0));
         } else if (line.rfind("Sleep", 0) == 0) {
             vector<string> param = split(line, '(');
             params.push_back(param.at(0));
-            params.push_back(removeclosing(param.at(1)));
+            params.push_back(split(param.at(1), ')').at(0));
         } else if (line.rfind("}", 0) == 0) {
             params.push_back("}");
         }
     }
-
+    file.close();
+    return params;
 }
 
 int main(int argc, char *argv[]) {
 
-    Tcp_Server server;
-    server.create_socket();
-    std::string filename = *argv;
-    lexerCode(filename);
+
+    std::string filename = argv[argc - 1];
+    vector<string> a;
+    try {
+        a = lexerCode(filename);
+    } catch (const char* e){
+        return -20;
+    }
+
+
+    for (string x : a) {
+        cout << x << ", " << endl;
+    }
+
 
     return 0;
 }
