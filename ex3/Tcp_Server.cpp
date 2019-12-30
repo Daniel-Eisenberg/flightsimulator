d7047ce42bfb3b613b16b9c6684df0c8665d5de6
Daniel-Eisenberg <dni1250@gmail.com>
2019-12-30 18:39:26 +0200

    silenced line 182 at command.cpp silenced the thread the opendataserver command at command.cpp

--- ex3/Tcp_Server.cpp
+++ ex3/Tcp_Server.cpp
@@ -3,6 +3,53 @@
 //
 
 #include "Tcp_Server.h"
+#include "Command.h"
+#include <string>
+#include <vector>
+#include "ex3.h"
+extern bool flag;
+
+std::vector<std::string> split(std::string str,std::string delimiter){
+
+    std::vector<std::string> arr;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = str.find(delimiter)) != std::string::npos) {
+        token = str.substr(0, pos);
+        if (token != "")
+            arr.push_back(token);
+        str.erase(0, pos + delimiter.length());
+    }
+    arr.push_back(str);
+    return arr;
+}
+
+
+
+
+char* getline(int fd){
+    int index=0, check;
+    char c;
+    char* line = (char*)malloc(10 * sizeof(char));
+    check = read(fd, &c, 1);
+
+    while(c != '\n' && check == 1){
+        if((index+1) % 10 == 0){
+            line = (char*)realloc(line, index+11);
+        }
+        line[index] = c;
+        check = read(fd, &c, 1);
+        index++;
+
+    }
+    if((index+1) % 10 == 0){
+        line = (char*)realloc(line, index+11);
+    }
+    line[index] = '\0';
+    return line;
+}
+
+
 
 int Tcp_Server::create_socket(int port) {
 
@@ -29,7 +76,6 @@ int Tcp_Server::create_socket(int port) {
             std::cerr << "Error during listening" << std::endl;
             return -3;
         }
-
         while (true) {
             //accept client
             int client_socket = accept(socket1, (struct sockaddr *) &address, (socklen_t *) &address);
@@ -37,13 +83,12 @@ int Tcp_Server::create_socket(int port) {
                 std::cerr << "Error accepting client" << std::endl;
                 return -4;
             }
-
-            char buffer[1024] = {0};
-            int read_client = read(client_socket, buffer, 1024);
-            std::cout << buffer << std::endl;
-            char *hello = "I can hear you \n";
-            send(client_socket, hello, strlen(hello), 0);
-            std::cout << "message sent\n" << std::endl;
+            char* line = getline(client_socket);
+            vector<string> values = split(line, " ");
+            //add the values to the map
+            sleep(5);
+            if (flag)
+                flag = false;
         }
 
 
