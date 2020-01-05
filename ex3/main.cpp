47649e050bdeda60855fc4706c88874dfabc708f
Daniel-Eisenberg <dni1250@gmail.com>
2020-01-05 03:41:16 +0200

    added comments and moved the flags to the client and server classes.

--- ex3/main.cpp
+++ ex3/main.cpp
@@ -17,18 +17,12 @@ int main(int argc, char *argv[]) {
         return -20;
     }
 
-
-    for (string &x : a) {
-        cout << x << endl;
-    }
-
     Parser::parser(&a, 0, false, 0);
     std::unique_lock<std::mutex> ul(Command::lock);
-    Command::killClientThread(0);
-    Command::cv.wait(ul, [] {return !Command::getKillClientThread();});
-    Command::killServerThread(0);
-    Command::cv.wait(ul, [] {return !Command::getKillServerThread();});
-
+    Client_Side::killClientThread(0);
+    Command::cv.wait(ul, [] {return !Client_Side::getKillClientThread();});
+    Tcp_Server::killServerThread(0);
+    Command::cv.wait(ul, [] {return !Tcp_Server::getKillServerThread();});
     Parser::clearMap();
 
     return 0;
